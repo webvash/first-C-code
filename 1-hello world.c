92b7b2ec0bc7039979e070db0518f92b1bbc8cd1
siyavash Rahimi <siavashelc@gmail.com>
2024-11-04 19:46:53 +0100

    add Calculation lines

--- 1-hello world.c
+++ 1-hello world.c
@@ -3,6 +3,7 @@ int main()
 {
    printf("Hello perfect word \n");
    printf("  *\n ***\n*****\n");
-  
+    printf(" Calculation\n");  
+    printf (" 5+2=%d\n 5-2=%d\n 5/2=%d\n 5%%2=%d\n",5+2,5-2,5/2,5%2);
    return 0;
 }
