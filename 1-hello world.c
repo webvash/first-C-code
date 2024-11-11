635ac72b1e6e4f8ea53b26ff96059688289c54e5
siyavash Rahimi <siavashelc@gmail.com>
2024-11-11 16:24:32 +0100

    add Rectangle of Asterisks line

--- 1-hello world.c
+++ 1-hello world.c
@@ -5,5 +5,7 @@ int main()
    printf("  *\n ***\n*****\n");
     printf(" Calculation\n");  
     printf (" 5+2=%d\n 5-2=%d\n 5/2=%d\n 5%%2=%d\n",5+2,5-2,5/2,5%2);
+     printf (" \n **********\n **********\n **********\n **********\n **********");
    return 0;
+   
 }
