e0257b445c0f1e395a91ecdc7d606b38545dc2f2
siyavash Rahimi <siavashelc@gmail.com>
2024-11-29 10:59:26 +0100

    Quadrant of a Point

--- 1-hello world.c
+++ 1-hello world.c
@@ -230,12 +230,25 @@ int main()
          else printf("Neither Double/Triple");
             */
          /////////////Control Flow--Print the Absolute Number ................
+         /*
          int num;
          printf("Enter num:");scanf("%d",&num);
          if (num>=0) printf ("absolute value=%d",num);
          else if (num<0) printf ("absolute value=%d",num*-1);
-         
+         */
+         /////////////Control Flow--Quadrant of a Point .....................
+         int x,y;
+         printf("Enter X:");scanf("%d",&x);
+         printf("Enter Y:");scanf("%d",&y);
+         if (x>0 && y>0) printf ("Quadrant 1");
+         else if (x<0 && y>0) printf ("Quadrant 2");
+         else if (x<0 && y<0) printf ("Quadrant 3");
+         else if (x>0 && y<0) printf ("Quadrant 4");
+         else printf ("on lines");
+             
+             
 
+ 
 
 
 
