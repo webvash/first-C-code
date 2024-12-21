290bad33ae40db5215908c289d2a76ce489ba703
siyavash Rahimi <siavashelc@gmail.com>
2024-12-21 20:48:18 +0100

    Maximum DIGIT in a 2 digits number

--- test2.c
+++ test2.c
@@ -10,9 +10,27 @@ float calculateArea(int h,int w)
 
  }
 
+ void TwoMaximumDIGITina2digitsnumber(num)
+ {
+    if (num>=10 && num<100)
+    {
+       int l,r;
+       l=num/10; r=num%10;
+      
+    
+       if (l>r) printf("Max digit=%d",l);
+       else if (l<r)  printf("Max digit=%d",r);
+        else  printf("Bothe number are equal");
+        
+    }
+    else printf("Please enter 2 digte number");
+
+ }
+
 int main ()
 {
     /////////////function---Find Rectangle Area.......................... 
+    /*
     int height,width;
     float area;
     printf("Enter height:"); scanf("%d",&height);
@@ -20,6 +38,18 @@ int main ()
 
    area=calculateArea(height,width);
    printf("Arae=%f",area);
+    */
+    /////////////function---2 Maximum DIGIT in a 2 digits number.......................... 
+    int num;
+    printf("Enter number:"); scanf("%d",&num);
+
+   TwoMaximumDIGITina2digitsnumber(num);
+
+
+
+
+
+
 
     return 0;
 }
