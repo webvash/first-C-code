db7994e08900ce47d994b1dc16c5a95cd17cbfa1
siyavash Rahimi <siavashelc@gmail.com>
2024-12-27 15:15:42 +0100

    Good Multiplication Neighbors

--- test3.c
+++ test3.c
@@ -21,14 +21,12 @@ int main ()
   printf("Max=%d",max);
  */
 /////////////array---Finding Maximum Index in Array.......................... 
-
+/*
 int num[7];    
 for (int i = 0; i < 7; i++)
 {
  printf ("Please enter the value index number %d:",i); scanf("%d",&num[i]);
 }
-
-
 int max=num[0],maxindex=0;
  for (int i = 1; i < 7; i++)
  {
@@ -36,14 +34,27 @@ int max=num[0],maxindex=0;
    {
        max=num[i];
        maxindex=i;
-   }
-   
+   } 
  }
  printf("Max=%d\n",max);
  printf("Max Index=%d",maxindex);
+*/
+/////////////array---Good Multiplication Neighbors.......................... 
+int num[7]={5,6,2,3,6,2,9};
+int ind=0;
+for (int i = 0; i < 6; i++)
+ {
+     if (num[i]*num[i+2]==num[i+1])
+  {
+   ind=1; break;
+  }
+ }
 
-
-
+if (ind==1)
+  {
+   printf ("Good Neighbor");
+  }
+else printf ("NOT Good Neighbor");
 
 
 
@@ -79,6 +90,8 @@ int max=num[0],maxindex=0;
 
 
 /////////////array---                       .......................... 
+
+
 
     return 0;
 }
