daa3d9e34f37bdf14557a2c858083d38f61a61eb
siyavash Rahimi <siavashelc@gmail.com>
2024-12-27 08:59:17 +0100

    Finding Maximum Index in Array

--- test3.c
+++ test3.c
@@ -5,7 +5,8 @@
 int main ()
 {
 
- /////////////function---Finding Maximum Value in Array.......................... 
+ /////////////array---Finding Maximum Value in Array.......................... 
+ /*
  int num[7]={1,4,0,2,9,4,5};    
  int max=num[0];
 
@@ -18,8 +19,66 @@ int main ()
     
   }
   printf("Max=%d",max);
+ */
+/////////////array---Finding Maximum Index in Array.......................... 
+
+int num[7];    
+for (int i = 0; i < 7; i++)
+{
+ printf ("Please enter the value index number %d:",i); scanf("%d",&num[i]);
+}
 
 
+int max=num[0],maxindex=0;
+ for (int i = 1; i < 7; i++)
+ {
+   if (num[i]>max)
+   {
+       max=num[i];
+       maxindex=i;
+   }
+   
+ }
+ printf("Max=%d\n",max);
+ printf("Max Index=%d",maxindex);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////array---                       .......................... 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////array---                       .......................... 
 
     return 0;
 }
