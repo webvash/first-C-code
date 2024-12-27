443e5fd300de650a73df4bb7c9da73862c9efdee
siyavash Rahimi <siavashelc@gmail.com>
2024-12-27 08:39:28 +0100

    Finding Maximum Value in Array

--- test3.c
+++ test3.c
@@ -5,7 +5,19 @@
 int main ()
 {
 
+ /////////////function---Finding Maximum Value in Array.......................... 
+ int num[7]={1,4,0,2,9,4,5};    
+ int max=num[0];
+
+  for (int i = 1; i < 7; i++)
+  {
+    if (num[i]>max)
+    {
+        max=num[i];
+    }
     
+  }
+  printf("Max=%d",max);
 
 
 
