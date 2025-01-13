a97bded0262bce0014c0a74d3524029b1fa072a7
siyavash Rahimi <siavashelc@gmail.com>
2025-01-13 15:37:29 +0100

    Input to Element in a 2D Array

--- test3.c
+++ test3.c
@@ -72,7 +72,7 @@ for (int i = 0; i < 3; i++)
 }
 */
 /////////////array---Multiplication Table Solution.......................... 
- 
+ /*
   //     1 2  3   4  5  6  7  8  9  10
   //1    1 2  3   4  5  6  7  8  9  10
   //2    2 4  6   8  10 12 14 16 18 20
@@ -96,7 +96,25 @@ for (int i = 0; i < 3; i++)
     printf("\n");
     
   }
+  */
+  /////////////array---Input to Element in a 2D Array.......................... 
+  double num[2][3];
+  for (int i = 0; i < 2; i++)
+  {
+     for (int j = 0; j < 3; j++)
+     {
+      printf("Please Enter Element number [%d][%d] ",i,j);  scanf("%lf",&num[i][j]);
+     }
+  }
   
+   for (int i = 0; i < 2; i++)
+ {
+    for (int j = 0; j < 3; j++)
+    {
+     printf("%.2lf ",num[i][j]);
+    }
+    printf("\n");
+ }
 
 
 
