8507c0f6ea5f3e0852ed253af19af63ccb339b93
siyavash Rahimi <siavashelc@gmail.com>
2025-01-15 13:06:19 +0100

    Guess what would be printed

--- test3.c
+++ test3.c
@@ -142,12 +142,40 @@ for (int i = 0; i < 3; i++)
      }
        */
   /////////////POINTER--- pointer..........................   
+   /*
      int grade1=50,grade2=80;
      printf("grade 1 value=%d\n",grade1);
       printf("grade 1 address=%p\n",&grade1);
      printf("grade 2 value=%d\n",grade2);
       printf("grade 2 address=%p\n",&grade2); 
+    */
+ /////////////POINTER---Guess what would be printed..........................   
+     
+     int num1 = 5, num2 = 7;
+    int *ptrA, *ptrB;
 
+    printf("num1 = %d, num2 = %d \n", num1, num2); // 5, 7
+
+    ptrA = &num1;
+    ptrB = &num2;
+
+    printf("num1 = %d, num2 = %d \n", num1, num2); // 5, 7
+
+    *ptrA = *ptrA + 1; // num1 = 6
+    *ptrB = *ptrB + 3; // num2 = 10
+
+    printf("num1 = %d, num2 = %d \n", num1, num2); // 6, 10
+
+    ptrA = ptrB; // ptrA -> num2, ptrB -> num2
+    ptrB = ptrA; // ptrB -> num2
+
+    printf("num1 = %d, num2 = %d \n", num1, num2); // 6, 10
+    printf("*ptrA = %d, *ptrB = %d \n", *ptrA, *ptrB); // 10, 10
+
+    num1 = *ptrB; // 10
+    num2 = num1 - 3; // 7
+
+    printf("num1 = %d, num2 = %d \n", num1, num2); // num1 = 10, num2 = 7
 
     
 
