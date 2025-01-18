6da7b23c47fa46982ec6ddeea05293557f09c732
siyavash Rahimi <siavashelc@gmail.com>
2025-01-18 16:43:28 +0100

    Arrays Copying an Array Question

--- test4.c
+++ test4.c
@@ -23,16 +23,33 @@ void findMinMax(int num1, int num2, int *pMax, int *pMin)
 int main ()
 {
 /////////////POINTER---Pass by Reference FULL EXAMPLE.......................... 
-
+  /*
     int a = 5, b = 7;
     int max, min;
     findMinMax(a, b, &max, &min);
     printf("max between %d and %d = %d\n", a, b, max);
     printf("min between %d and %d = %d\n", a, b, min);
+  */
+/////////////Arrays---Arrays Copying an Array Question.......................... 
+
+  int date [3]={1,2,2000};
+  int date2[3];
+ 
+ for (int i = 0; i < 3; i++)
+ {
+ printf ("%d ",date [i]);  date2[i]=date[i];
+ }
+printf ("\n"); 
+ 
+for (int i = 0; i < 3; i++)
+{
+printf ("%d ",date2 [i]); 
+}
+    
+  
 
 
 
 
     return 0;
 } 
-
