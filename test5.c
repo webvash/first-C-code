2cd2f7042b511a150d80af6b1beffc545d184ad7
siyavash Rahimi <siavashelc@gmail.com>
2025-02-20 10:51:05 +0100

    String Initialization

---
+++ test5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main ()
+{
+ /////////////String---String Initialization.......................... 
+    
+  char st[]="hello";
+  printf("%s\n", st);
+
+  char st1[]={'h','e','l','l','o','\0'};
+  printf("%s\n", st1);
+
+  printf("%c\n", st1[4]);
+ 
+/////////////String---                         .......................... 
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
+
+
